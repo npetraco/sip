e08ec38f433c8508633d1cbf2c1187af7fbacdeb
npetraco <npetraco@gmail.com>
2014-01-07 21:17:03 -0500

    slight change for XPtr problem

--- src/AddNode.cpp
+++ src/AddNode.cpp
@@ -8,13 +8,16 @@ using namespace std;
 // [[Rcpp::export]]
 SEXP AddNode(SEXP net_ptr, SEXP node_name, SEXP node_type, SEXP node_states) {
   
-  //DSL_network theNet = *net_ptr;
+  //as the pointer sent in to a Rcpp Xptr 
   Rcpp::XPtr<DSL_network> net_ptr_mod(net_ptr);
   
-  // create node
-  //int success = 1035;
-  int node_handel = net_ptr_mod->AddNode(DSL_CPT,"Success");
-  //Rcout<<"got back"<<success<<endl;
+  //as the node name to a C++ string
+  //std::vector<std::string> nde_nme;// = as<string>(node_name);
+  
+  // create node  
+  int node_handel = net_ptr_mod->AddNode(DSL_CPT,"SUCCESS");
+  //int node_handel = net_ptr_mod->AddNode(DSL_CPT,nde_nme);
+  //Rcout<<"got back "<< ndn <<endl;
   
   // setting number (and name) of outcomes
   //DSL_idArray someNames;
