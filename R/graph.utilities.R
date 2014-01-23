8e25421d269ebb6395c7d6682d27af03ba033954
npetraco <npetraco@gmail.com>
2014-01-23 08:25:56 -0500

    updated graph plotting

--- R/graph.utilities.R
+++ R/graph.utilities.R
@@ -13,3 +13,37 @@ adjacency.matrix<-function(node.names, arc.mat) {
   return(adj.mat)
   
 }
+
+
+show.network<-function(network.pointer){
+  
+  network.info<-network.info(network.pointer)
+  nodes.names <- network.info$node.info[,"node.name"]
+  node.types <- network.info$node.info[,2]
+  
+  adjac.mat <- adjacency.matrix(nodes.names, network.info$arcs)
+  graph.obj <- as(adjac.mat, "graphNEL")
+  
+  nodes.info <- buildNodeList(graph.obj)
+  edges.info <- buildEdgeList(graph.obj)
+  
+  #Set attributes (shape/color) for the different types of nodes 
+  for (i in 1:length(nodes.info)) {
+    if(node.types[i] == "utility") {
+      nodes.info[[i]]@attrs$shape <- "circle"
+      nodes.info[[i]]@attrs$fillcolor <- "green"
+    }
+    if(node.types[i] == "decision") {
+      nodes.info[[i]]@attrs$shape <- "box"
+      nodes.info[[i]]@attrs$fillcolor <- "red"
+    }
+    if(node.types[i] == "chance") {
+      nodes.info[[i]]@attrs$shape <- "ellipse"
+      nodes.info[[i]]@attrs$fillcolor <- "yellow"
+    }  
+  }
+  
+  graph.obj.prettier <- agopen(graph.obj, edges=edges.info, nodes=nodes.info, name="pretty graph")
+  plot(graph.obj.prettier)
+  
+}
--- tests/apple_id4.R
+++ tests/apple_id4.R
@@ -2,45 +2,12 @@ library(sip)
 #library(gRbase)
 library(Rgraphviz)
 
+#We want an graphNEL to newtork function
+
 np<-create.network()
 read.network(np, "/Users/npetraco/codes/R/sip/tests/apple.net", "hugin")
-np.info<-network.info(np)
-node.types <- np.info$node.info[,2]
+show.network(np)
 
-nodes <- np.info$node.info[,"node.name"]
-adjacm <- adjacency.matrix(nodes, np.info$arcs)
-gr <- as(adjacm, "graphNEL")
-plot(gr, "neato")
-plot(gr, "circo")
-plot(gr, "fdp")
-plot(gr, "dot")
-plot(gr, "twopi")
-plot(gr)
-plot(gr, attrs=list(node = list(fillcolor="lightgrey", fontcolor="red")))
-dev.off()
+network.info(np)
 
-nodes <- buildNodeList(gr)
-nodes
-edges <- buildEdgeList(gr)
-edges
-
-for (i in 1:length(nodes)) {
-  
-  if(node.types[i] == "utility") {
-    nodes[[i]]@attrs$shape <- "circle"
-    nodes[[i]]@attrs$fillcolor <- "green"
-  }
-  if(node.types[i] == "decision") {
-    nodes[[i]]@attrs$shape <- "box"
-    nodes[[i]]@attrs$fillcolor <- "red"
-  }
-  if(node.types[i] == "chance") {
-    nodes[[i]]@attrs$shape <- "ellipse"
-    nodes[[i]]@attrs$fillcolor <- "yellow"
-  }
-  
-}
-
-gr2 <- agopen(gr, edges=edges, nodes=nodes, name="gr2")
-plot(gr2)
 dev.off()
